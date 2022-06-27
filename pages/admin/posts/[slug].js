72fedd3e0178d5c94e516319b32e581160f68820
bbissm <martin.bissiger@hotmail.com>
2022-06-27 23:27:58 +0200

    added directory path for admin/posts

---
+++ pages/admin/posts/[slug].js
@@ -0,0 +1,12 @@
+import Metatags from "../../../components/Metatags";
+
+function AdminPostEdit() {
+    return (
+        <main>
+            <Metatags title="admin page" description="This is an admin page with usual content"/>
+            <h1>Edit Post</h1>
+        </main>
+    );
+}
+
+export default AdminPostEdit;
