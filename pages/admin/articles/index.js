74a3a27ae69524090b80ba2e6490633ebf013213
Martin Bissiger <martin.bissiger@hotmail.com>
2023-01-04 21:42:10 +0100

    Change useeffect to serversideprops

--- pages/admin/articles/index.js
+++ pages/admin/articles/index.js
@@ -2,27 +2,37 @@ import { useEffect, useState } from "react";
 import AuthCheck from "../../../components/AuthCheck";
 
 export default function AdminPostsPage(props) {
+  const { data } = props;
 
-  const [articles, setArticles] = useState([]);
-
-  useEffect(() => {
-    fetch('http://restapi.loc:88/articles')
-      .then((res) => res.json())
-      .then((data) => {
-        setArticles(data)
-      })
-  }, [])
 
     return (
       <main>
         <AuthCheck>
           <h1 className="mb-6">All Articles</h1>
-          {articles.map((article, index) => {
+          {data.map((article, index) => {
               return (
-                <h2>{article.id}. {article.title}</h2>
+                <h2 key={article.id}>{article.id}. {article.title}</h2>
               )
           })}
         </AuthCheck>
       </main>
     );
   }
+
+  export async function getServerSideProps() {
+    try {
+      const response = await fetch("http://restapi.loc:88/articles");
+      const data = await response.json();
+  
+      return {
+        props: {
+          data,
+        },
+      };
+    } catch (error) {
+      console.log(error);
+      return {  props: { data: [] }  };
+    }
+  }
+
+  
